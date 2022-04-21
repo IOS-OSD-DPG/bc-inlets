abf70b9d74eb16a19787570b80d2c32a3f8555a8
James Hannah <jhannah@protonmail.com>
2022-04-21 11:06:54 -0700

    add new map and section for Howe Sound

--- get-maps.py
+++ get-maps.py
@@ -30,6 +30,7 @@ maps = [
     Inlet(boundaries=[-127.9, 51.8, -127.2, 52.4], dimensions="600x600", name="Burke Channel"),
     Inlet(boundaries=[-130.2, 54.8, -129.7, 55.4], dimensions="500x700", name="Observatory Inlet"),
     Inlet(boundaries=[-126.7, 49.5, -126.0, 49.7], dimensions="700x400", name="Muchalat Inlet"),
+    Inlet(boundaries=[-123.4, 49.3, -123.2, 49.7], dimensions="500x700", name="Howe Sound"),
 ]
 
 mapbox_url = "https://api.mapbox.com/styles/v1/cyborgsphinx/{}/static/{}/{}"
